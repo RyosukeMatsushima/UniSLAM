fc42c6c30b5f24ffa40b4e24cc766addc2634b55
RyosukeMatsushima <m.ryosuke@me.com>
2024-05-22 21:20:43 +0900

    getable distance and angle between edge points

--- test/edge_point_test.cpp
+++ test/edge_point_test.cpp
@@ -55,4 +55,17 @@ TEST(EgdePointTest, IsContinuous)
     EXPECT_FALSE(edge_point.isContinuous(new_edge_point, false));
 }
 
+TEST(EgdePointTest, GetDiffToMatchedPoint)
+{
+    cv::Point2f point(1, 2);
+    cv::Vec2f gradient(3, 4);
+    EdgePoint edge_point(point, gradient);
+
+    cv::Point2f new_point(1.5, 2.5);
+    cv::Vec2f new_gradient(3.5, 4.5);
+    EdgePoint new_edge_point(new_point, new_gradient);
+
+    EXPECT_FLOAT_EQ(edge_point.distanceTo(new_edge_point), cv::norm(new_point - point));
+    EXPECT_FLOAT_EQ(edge_point.angleTo(new_edge_point), atan2(new_point.y - point.y, new_point.x - point.x));
+}
 
