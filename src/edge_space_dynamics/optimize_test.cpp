8181ff1193e76ba70d862e2e5effeac318af61aa
RyosukeMatsushima <m.ryosuke@me.com>
2024-06-23 15:07:59 +0900

    fix optimize_test

--- src/edge_space_dynamics/optimize_test.cpp
+++ src/edge_space_dynamics/optimize_test.cpp
@@ -20,6 +20,7 @@ public:
     Eigen::Vector3f start_point;
     Eigen::Vector3f direction;
     float length;
+    int id;
 
     const Eigen::Vector3f correct_start_point;
     const Eigen::Vector3f correct_direction;
@@ -37,6 +38,7 @@ protected:
     FrameData frame0;
     FrameData frame1;
     FrameData frame2;
+    FrameData frame3;
 
     EdgeData edge0;
     EdgeData edge1;
@@ -45,33 +47,39 @@ protected:
 
     OptimizeTest()
         : frame0(Pose3D(), {
-            EdgeNode(Eigen::Vector3f(1.0, 0.0, 1.0), Eigen::Vector2f(1.0, 0.0), 0),
-            EdgeNode(Eigen::Vector3f(-1.0, 0.0, 1.0), Eigen::Vector2f(1.0, 0.0), 1),
-            EdgeNode(Eigen::Vector3f(0.0, 1.0, 1.0), Eigen::Vector2f(0.0, 1.0), 2),
-            EdgeNode(Eigen::Vector3f(0.0, -1.0, 1.0), Eigen::Vector2f(0.0, 1.0), 3)
+            EdgeNode(Eigen::Vector3f(1.0, 0.0, 1.0), Eigen::Vector2f(0.0, 1.0), 0),
+            EdgeNode(Eigen::Vector3f(-1.0, 0.0, 1.0), Eigen::Vector2f(0.0, 1.0), 1),
+            EdgeNode(Eigen::Vector3f(0.0, 1.0, 1.0), Eigen::Vector2f(1.0, 0.0), 2),
+            EdgeNode(Eigen::Vector3f(0.0, -1.0, 1.0), Eigen::Vector2f(1.0, 0.0), 3)
           }),
-          frame1(Pose3D(), {
-            EdgeNode(Eigen::Vector3f(0.0, 0.0, 1.0), Eigen::Vector2f(1.0, 0.0), 0),
-            EdgeNode(Eigen::Vector3f(-2.0, 0.0, 1.0), Eigen::Vector2f(1.0, 0.0), 1),
-            EdgeNode(Eigen::Vector3f(0.0, 1.0, 1.0), Eigen::Vector2f(0.0, 1.0), 2),
-            EdgeNode(Eigen::Vector3f(0.0, -1.0, 1.0), Eigen::Vector2f(0.0, 1.0), 3)
+          frame1(Pose3D(Eigen::Vector3f(1.0, 0.0, 0.0), Eigen::Quaternionf::Identity()), {
+            EdgeNode(Eigen::Vector3f(0.0, 0.0, 1.0), Eigen::Vector2f(0.0, 1.0), 0),
+            EdgeNode(Eigen::Vector3f(-2.0, 0.0, 1.0), Eigen::Vector2f(0.0, 1.0), 1),
+            EdgeNode(Eigen::Vector3f(0.0, 1.0, 1.0), Eigen::Vector2f(1.0, 0.0), 2),
+            EdgeNode(Eigen::Vector3f(0.0, -1.0, 1.0), Eigen::Vector2f(1.0, 0.0), 3)
           }),
-          frame2(Pose3D(), {
-            EdgeNode(Eigen::Vector3f(2.0, 0.0, 1.0), Eigen::Vector2f(1.0, 0.0), 0),
-            EdgeNode(Eigen::Vector3f(0.0, 0.0, 1.0), Eigen::Vector2f(1.0, 0.0), 1),
-            EdgeNode(Eigen::Vector3f(0.0, 1.0, 1.0), Eigen::Vector2f(0.0, 1.0), 2),
-            EdgeNode(Eigen::Vector3f(0.0, -1.0, 1.0), Eigen::Vector2f(0.0, 1.0), 3)
+          frame2(Pose3D(Eigen::Vector3f(-1.0, 0.0, 0.0), Eigen::Quaternionf::Identity()), {
+            EdgeNode(Eigen::Vector3f(2.0, 0.0, 1.0), Eigen::Vector2f(0.0, 1.0), 0),
+            EdgeNode(Eigen::Vector3f(0.0, 0.0, 1.0), Eigen::Vector2f(0.0, 1.0), 1),
+            EdgeNode(Eigen::Vector3f(0.0, 1.0, 1.0), Eigen::Vector2f(1.0, 0.0), 2),
+            EdgeNode(Eigen::Vector3f(0.0, -1.0, 1.0), Eigen::Vector2f(1.0, 0.0), 3)
           }),
-          edge0(Eigen::Vector3f(1.0, 0.0, 1.0), Eigen::Vector3f(1.0, 0.0, 0.0), 1.0),
-          edge1(Eigen::Vector3f(-1.0, 0.0, 1.0), Eigen::Vector3f(1.0, 0.0, 0.0), 1.0),
-          edge2(Eigen::Vector3f(0.0, 1.0, 1.0), Eigen::Vector3f(0.0, 1.0, 0.0), 1.0),
-          edge3(Eigen::Vector3f(0.0, -1.0, 1.0), Eigen::Vector3f(0.0, 1.0, 0.0), 1.0) {}
+          frame3(Pose3D(Eigen::Vector3f(0.0, 1.0, 0.0), Eigen::Quaternionf::Identity()), {
+            EdgeNode(Eigen::Vector3f(1.0, 0.0, 1.0), Eigen::Vector2f(0.0, 1.0), 0),
+            EdgeNode(Eigen::Vector3f(-1.0, 0.0, 1.0), Eigen::Vector2f(0.0, 1.0), 1),
+            EdgeNode(Eigen::Vector3f(0.0, 0.0, 1.0), Eigen::Vector2f(1.0, 0.0), 2),
+            EdgeNode(Eigen::Vector3f(0.0, -2.0, 1.0), Eigen::Vector2f(1.0, 0.0), 3)
+          }),
+          edge0(Eigen::Vector3f(1.0, 0.0, 1.0), Eigen::Vector3f(0.0, 1.0, 0.0), 1.0),
+          edge1(Eigen::Vector3f(-1.0, 0.0, 1.0), Eigen::Vector3f(0.0, 1.0, 0.0), 1.0),
+          edge2(Eigen::Vector3f(0.0, 1.0, 1.0), Eigen::Vector3f(1.0, 0.0, 0.0), 1.0),
+          edge3(Eigen::Vector3f(0.0, -1.0, 1.0), Eigen::Vector3f(1.0, 0.0, 0.0), 1.0) {}
 
     void setEdges() {
-        edge_space_dynamics.set_edge3d(edge0.start_point, edge0.direction, edge0.length);
-        edge_space_dynamics.set_edge3d(edge1.start_point, edge1.direction, edge1.length);
-        edge_space_dynamics.set_edge3d(edge2.start_point, edge2.direction, edge2.length);
-        edge_space_dynamics.set_edge3d(edge3.start_point, edge3.direction, edge3.length);
+        edge0.id = edge_space_dynamics.set_edge3d(edge0.start_point, edge0.direction, edge0.length);
+        edge1.id = edge_space_dynamics.set_edge3d(edge1.start_point, edge1.direction, edge1.length);
+        edge2.id = edge_space_dynamics.set_edge3d(edge2.start_point, edge2.direction, edge2.length);
+        edge3.id = edge_space_dynamics.set_edge3d(edge3.start_point, edge3.direction, edge3.length);
     }
 
     void addNoise(FrameData& frame, Eigen::Vector3f translation_noise, Eigen::Vector3f rotation_noise) {
@@ -97,32 +105,29 @@ protected:
     }
 
     void checkEdgeData(const EdgeData& edge, float translation_error_threshold = 1e-4, float rotation_error_threshold = 1e-4) {
-        EXPECT_NEAR(edge.start_point.x(), edge.correct_start_point.x(), translation_error_threshold);
-        EXPECT_NEAR(edge.start_point.y(), edge.correct_start_point.y(), translation_error_threshold);
-        EXPECT_NEAR(edge.start_point.z(), edge.correct_start_point.z(), translation_error_threshold);
-
-        EXPECT_NEAR(edge.direction.x(), edge.correct_direction.x(), rotation_error_threshold);
-        EXPECT_NEAR(edge.direction.y(), edge.correct_direction.y(), rotation_error_threshold);
-        EXPECT_NEAR(edge.direction.z(), edge.correct_direction.z(), rotation_error_threshold);
-
-        EXPECT_NEAR(edge.length, edge.correct_length, translation_error_threshold);
+        EXPECT_NEAR(edge_space_dynamics.get_edge3d(edge.id).direction().x(), edge.correct_direction.x(), translation_error_threshold);
+        EXPECT_NEAR(edge_space_dynamics.get_edge3d(edge.id).direction().y(), edge.correct_direction.y(), translation_error_threshold);
+        EXPECT_NEAR(edge_space_dynamics.get_edge3d(edge.id).direction().z(), edge.correct_direction.z(), translation_error_threshold);
     }
 };
 
 TEST_F(OptimizeTest, optimizeWithoutNoise) {
-    int max_iterations = 100;
+    int max_iterations = 1000;
 
-    addNoise(frame0, Eigen::Vector3f(1.0f, -1.0f, -1.0f), Eigen::Vector3f(-1.1f, 0.2f, 0.2f));
+    addNoise(frame0, Eigen::Vector3f(0.2f, 0.1f, 0.3f), Eigen::Vector3f(0.1f, 0.2f, 0.2f));
 
-    addNoise(edge0, Eigen::Vector3f(0.1f, 0.1f, 0.1f), Eigen::Vector3f(0.1f, 0.1f, 0.1f), 0.1f);
-    addNoise(edge1, Eigen::Vector3f(0.1f, 0.1f, 0.1f), Eigen::Vector3f(0.1f, 0.1f, 0.1f), 0.1f);
-    addNoise(edge2, Eigen::Vector3f(0.1f, 0.1f, 0.1f), Eigen::Vector3f(0.1f, 0.1f, 0.1f), 0.1f);
-    addNoise(edge3, Eigen::Vector3f(0.1f, 0.1f, 0.1f), Eigen::Vector3f(0.1f, 0.1f, 0.1f), 0.1f);
+    addNoise(edge0, Eigen::Vector3f(0.1f, 0.1f, 0.1f), Eigen::Vector3f(0.4f, -0.1f, 1.1f), 0.1f);
+    addNoise(edge1, Eigen::Vector3f(-0.1f, 0.1f, -0.1f), Eigen::Vector3f(-1.1f, -0.1f, 0.4f), 0.1f);
+    addNoise(edge2, Eigen::Vector3f(0.3f, 1.1f, 0.1f), Eigen::Vector3f(1.1f, 0.3f, -0.3f), 0.1f);
+    addNoise(edge3, Eigen::Vector3f(0.1f, -1.1f, 0.1f), Eigen::Vector3f(-0.1f, -0.4f, 0.7f), 0.1f);
+
+    setEdges();
 
     for (int i = 0; i < max_iterations; i++) {
         EXPECT_TRUE(edge_space_dynamics.optimize(frame0.pose, frame0.edge_nodes, true));
         EXPECT_TRUE(edge_space_dynamics.optimize(frame1.pose, frame1.edge_nodes, false));
         EXPECT_TRUE(edge_space_dynamics.optimize(frame2.pose, frame2.edge_nodes, false));
+        EXPECT_TRUE(edge_space_dynamics.optimize(frame3.pose, frame3.edge_nodes, false));
     }
 
     checkFrameData(frame0);
