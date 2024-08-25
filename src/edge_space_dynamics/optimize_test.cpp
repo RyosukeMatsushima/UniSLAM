4ccaac8297869ddb39422522e2e3bacac9c0e878
RyosukeMatsushima <m.ryosuke@me.com>
2024-08-25 18:58:43 +0900

    separate edge_control_test

--- src/edge_space_dynamics/optimize_test.cpp
+++ src/edge_space_dynamics/optimize_test.cpp
@@ -254,32 +254,6 @@ TEST_F(OptimizeTest, joinEdges) {
     checkAllData();
 }
 
-TEST(EdgeControlTest, setGetRemoveEdge) {
-    EdgeSpaceDynamics edge_space_dynamics(CONFIG_YAML_PATH);
-
-    Eigen::Vector3f start_point(1.0, 0.0, 1.0);
-    Eigen::Vector3f direction(0.0, 1.0, 0.0);
-
-    int edge_num = 5;
-
-    for (int i = 0; i < edge_num; i++) {
-        float length = float(i);
-        int edge_id = edge_space_dynamics.set_edge3d(start_point, direction, length);
-        EXPECT_EQ(edge_id, i);
-        EXPECT_EQ(edge_space_dynamics.get_edge3d(edge_id).length(), length);
-    }
-
-    int remove_edge_id = 2;
-    edge_space_dynamics.remove_edge3d(remove_edge_id);
-
-    EXPECT_THROW(edge_space_dynamics.get_edge3d(remove_edge_id), std::invalid_argument);
-
-    for (int i = 0; i < edge_num - 1; i++) {
-        if (i == remove_edge_id) continue;
-        EXPECT_EQ(edge_space_dynamics.get_edge3d(i).length(), float(i));
-    }
-}
-
 int main(int argc, char **argv) {
     testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
